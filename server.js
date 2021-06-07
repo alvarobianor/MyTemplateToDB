4c845ac99a2044af6e82e963626effd9853568ad
alvaro bianor <bianor.alvaro12@gmail.com>
2021-06-07 18:50:50 -0300

    Test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3333;
-
+const a = 2;
 server.use(middlewares);
 server.use(router);
 
